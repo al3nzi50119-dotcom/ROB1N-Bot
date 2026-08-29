ac23c79cafb5e7a42f940f274d3112b07670fd85
al3nzi50119-dotcom <al3nzi50119@gmail.com>
2026-08-29 17:15:03 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "fawazamjasim",     // this is what people actually see
+        state: "ROB1N was watching",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
